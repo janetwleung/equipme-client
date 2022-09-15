46ae6dc2a8537688c5de4c4707b488795f20842e
Janet Leung <leung.janetw@hotmail.com>
2022-09-15 14:41:53 -0400

    Create mixins for link reset and list reset as well as breakpoints partial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,24 +3,41 @@ import { Link, NavLink } from "react-router-dom";
 
 function Header() {
     return (
-        <header classNAme="header">
-            <Link to="/"><h1 className="header__title">EquipMe</h1></Link>
+        <header className="header">
+            <Link to="/" className="header__title-link"><h1 className="header__title">EquipMe</h1></Link>
             <ul className="header__nav">
                 <li className="header__nav-item">
-                    <NavLink to="/pros">Pros</NavLink>
+                    <NavLink to="/pros" className="header__nav-item-link">Pros</NavLink>
                 </li>
                 <li className="header__nav-item">
-                    <NavLink to="/used">Used</NavLink>
+                    <NavLink to="/used" className="header__nav-item-link">Used</NavLink>
                 </li>
                 <li className="header__nav-item">
-                    <NavLink to="/signup">Sign up</NavLink>
+                    <NavLink to="/signup" className="header__nav-item-link">Sign up</NavLink>
                 </li>
                 <li className="header__nav-item">
-                    <NavLink to="login">Login</NavLink>
+                    <NavLink to="login" className="header__nav-item-link">Login</NavLink>
                 </li>
             </ul>
+
         </header>
     );
 }
 
 export default Header;
+
+
+{/* <ul className="header__nav">
+                <li className="header__nav-item">
+                    <NavLink to="/pros" className="header__nav-item-link">Pros</NavLink>
+                </li>
+                <li className="header__nav-item">
+                    <NavLink to="/used" className="header__nav-item-link">Used</NavLink>
+                </li>
+                <li className="header__nav-item">
+                    <NavLink to="/signup" className="header__nav-item-link">Sign up</NavLink>
+                </li>
+                <li className="header__nav-item">
+                    <NavLink to="login" className="header__nav-item-link">Login</NavLink>
+                </li>
+            </ul> */}
