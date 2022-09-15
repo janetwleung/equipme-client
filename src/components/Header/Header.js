4667e54be09d93d3df3111e05349cb100c581fd5
Janet Leung <leung.janetw@hotmail.com>
2022-09-15 15:38:45 -0400

    Puts header in a div to add padding, adds border bottom

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,22 +5,23 @@ import CTA from "../CTA/CTA";
 function Header() {
     return (
         <header className="header">
-            <Link to="/" className="header__title-link"><h1 className="header__title">EquipMe</h1></Link>
-            <ul className="header__nav">
-                <li className="header__nav-item">
-                    <NavLink to="/pros" className="header__nav-item-link">Pros</NavLink>
-                </li>
-                <li className="header__nav-item">
-                    <NavLink to="/used" className="header__nav-item-link">Used</NavLink>
-                </li>
-                <li className="header__nav-item">
-                    <CTA isButton={false} text="Sign Up" link="/signup"/>
-                </li>
-                <li className="header__nav-item">
-                    <CTA isButton={false} text="Login" type="secondary" link="/login"/>
-                </li>
-            </ul>
-
+            <div className="header__container">
+                <Link to="/" className="header__title-link"><h1 className="header__title">EquipMe</h1></Link>
+                <ul className="header__nav">
+                    <li className="header__nav-item">
+                        <NavLink to="/pros" className="header__nav-item-link">Pros</NavLink>
+                    </li>
+                    <li className="header__nav-item">
+                        <NavLink to="/used" className="header__nav-item-link">Used</NavLink>
+                    </li>
+                    <li className="header__nav-item">
+                        <CTA isButton={false} text="Sign Up" link="/signup"/>
+                    </li>
+                    <li className="header__nav-item">
+                        <CTA isButton={false} text="Login" type="secondary" link="/login"/>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
