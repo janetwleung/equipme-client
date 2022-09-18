f914841acfeff340f94bc7f42c524e9a8e8b48ff
Janet Leung <leung.janetw@hotmail.com>
2022-09-18 14:27:29 -0400

    Adds hamburger menu toggle to navbar. Adds nav title, sets h1 font on App.scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,26 +3,50 @@ import { Link, NavLink } from "react-router-dom";
 import CTA from "../CTA/CTA";
 
 function Header() {
+
+
     return (
-        <header className="header">
-            <div className="header__container">
-                <Link to="/" className="header__title-link"><h1 className="header__title">EquipMe.</h1></Link>
-                <ul className="header__nav">
-                    <li className="header__nav-item">
-                        <NavLink to="/pros" className="header__nav-item-link">Pros</NavLink>
-                    </li>
-                    <li className="header__nav-item">
-                        <NavLink to="/used" className="header__nav-item-link">Used</NavLink>
-                    </li>
-                    <li className="header__nav-item">
-                        <CTA isButton={false} text="Sign Up" link="/signup"/>
-                    </li>
-                    <li className="header__nav-item">
-                        <CTA isButton={false} text="Login" type="secondary" link="/login"/>
-                    </li>
-                </ul>
-            </div>
-        </header>
+        <>
+            <header className="mobile-header">
+                <div className="mobile-header__content">
+                    <nav className="mobile-header__navigation" role="navigation">
+                        <div className="mobile-header__menu-toggle" id="menuToggle">
+                            <input type="checkbox" />
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <ul className="mobile-header__list" id="menu">
+                                <li className="mobile-header__list-item">What The Pros Wear</li>
+                                <li className="mobile-header__list-item">Shop Used</li>
+                                <li className="mobile-header__list-item">Login</li>
+                                <li className="mobile-header__list-item">Sign Up</li>
+                            </ul>
+                        </div>
+                        <h1 className="mobile-header__title">EquipMe.</h1>
+                    </nav>
+                </div>
+            </header>
+            
+            <header className="header">
+                <div className="header__container">
+                    <Link to="/" className="header__title-link"><h1 className="header__title">EquipMe.</h1></Link>
+                    <ul className="header__nav">
+                        <li className="header__nav-item">
+                            <NavLink to="/pros" className="header__nav-item-link">Pros</NavLink>
+                        </li>
+                        <li className="header__nav-item">
+                            <NavLink to="/used" className="header__nav-item-link">Used</NavLink>
+                        </li>
+                        <li className="header__nav-item">
+                            <CTA isButton={false} text="Sign Up" link="/signup" />
+                        </li>
+                        <li className="header__nav-item">
+                            <CTA isButton={false} text="Login" type="secondary" link="/login" />
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </>
     );
 }
 
