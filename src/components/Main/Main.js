fce872b7b6466b28d0ff0af974b9f191f2a39e1b
Janet Leung <leung.janetw@hotmail.com>
2022-09-18 21:00:34 -0400

    Created hero background for mobile, tablet and desktop screen sizes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,12 @@ import "./Main.scss";
 function Main() {
     return (
         <main className="main">
+            <div className="main__hero-background-top">
+                Sports
+            </div>
+            <div className="main__hero-background-bottom">
+                Sports
+            </div>  
             <div className="main__hero">
                 EquipMe.
             </div>
