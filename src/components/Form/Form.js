f059adfddabf764e539fc2253accf019a1d82b86
Janet Leung <leung.janetw@hotmail.com>
2022-09-19 17:30:59 -0400

    Created basic form elements in form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,33 @@
+import CTA from "../CTA/CTA";
 import "./Form.scss";
 
 function Form() {
     return (
         <main className="intake">
-            <h2 className="intake__title">Tell us about yourself</h2>
-            <form>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-            </form>
+            <div className="intake__container">
+                <h2 className="intake__title">Tell us about yourself</h2>
+                <form className="intake__form">
+                    <div className="intake__input-container">
+                        <label htmlFor="age" className="intake__label">Age:</label>
+                        <input className="intake__input" type="text" id="age" name="age" />
+                    </div>
+                    <div className="intake__input-container">
+                        <label htmlFor="height" className="intake__label">Height:</label>
+                        <input className="intake__input" type="text" id="height" name="height" />
+                    </div>
+                    <div className="intake__input-container">
+                        <label htmlFor="position" className="intake__label">Position:</label>
+                        <input className="intake__input" type="text" id="position" name="position" />
+                    </div>
+                    <div className="intake__input-container">
+                        <label htmlFor="level" className="intake__label">Level:</label>
+                        <input className="intake__input" type="text" id="level" name="level" />
+                    </div>
+                    <div className="intake__button">
+                        <CTA text="Submit" />
+                    </div>
+                </form>
+            </div>
         </main>
     );
 }
