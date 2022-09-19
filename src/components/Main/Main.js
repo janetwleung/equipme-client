d9aeb88637c8faaa22110b50963e60217833b617
Janet Leung <leung.janetw@hotmail.com>
2022-09-18 21:32:31 -0400

    Adds title to hero section. Mobile and tablet styles complete

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,15 +3,15 @@ import "./Main.scss";
 function Main() {
     return (
         <main className="main">
-            <div className="main__hero-background-top">
-                Sports
-            </div>
-            <div className="main__hero-background-bottom">
-                Sports
+            <section className="main__hero">
+            <div className="main__hero-background">
+                <div className="main__hero-background-top">Sports</div>
+                <div className="main__hero-background-bottom">Sports</div>
             </div>  
-            <div className="main__hero">
+            <div className="main__title">
                 EquipMe.
             </div>
+            </section>
         </main>
     );
 }
