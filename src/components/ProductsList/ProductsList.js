c04d73a4b19b7e1f421790c632a8940ab6114e5b
Janet Leung <leung.janetw@hotmail.com>
2022-09-20 14:07:03 -0400

    Add ids to each item in json files

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,16 +1,14 @@
 import ProductCard from "../ProductCard/ProductCard";
 import "./ProductsList.scss";
-import { v4 as uuidv4 } from 'uuid';
 
 function ProductsList({ products }) {
     return (
         <ul className="products-list">
             {/* Use prop to map through list of gloves/bats/cleats below using ProductCard component */}
                 {products.map(product => (
-                <li>
+                <li key={product.id}>
                     <ProductCard
-                        key={uuidv4()}
-                        id={uuidv4()}
+                        id={product.id}
                         product={product}
                         brand={product.brand}
                         name={product.name}
