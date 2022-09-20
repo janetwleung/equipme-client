676619d318d612ae85d0cc6932631161ab0c951f
Janet Leung <leung.janetw@hotmail.com>
2022-09-19 22:03:56 -0400

    Removes ProductCard from ProductsPage

--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -1,4 +1,3 @@
-import ProductCard from "../../components/ProductCard/ProductCard";
 import ProductsNav from "../../components/ProductsNav/ProductsNav";
 import "./ProductsPage.scss";
 
@@ -6,7 +5,6 @@ function ProductsPage({ product }) {
     return (
         <main className="products">
             <ProductsNav />
-            <ProductCard />
         </main>
     );
 }
