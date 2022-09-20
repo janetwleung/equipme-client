49de38f5e9b3e396f5fefa3bce7f956cf9fc937f
Janet Leung <leung.janetw@hotmail.com>
2022-09-19 22:57:32 -0400

    Minor changes, wrong feature branch

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -2,25 +2,26 @@ import ProductCard from "../ProductCard/ProductCard";
 import "./ProductsList.scss";
 
 function ProductsList({ product }) {
+
     return (
         <ul className="products-list">
             {/* Use prop to map through list of gloves/bats/cleats below using ProductCard component */}
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
-            <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
+                <li><ProductCard /></li>
         </ul>
     );
 }
