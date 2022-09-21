23d578672da745b8852e4f7fd3517304c9874b5f
Janet Leung <leung.janetw@hotmail.com>
2022-09-20 20:07:04 -0400

    Creates api-utils file to house API get requests for gloves, bat, and cleats endpoints

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Form from './components/Form/Form';
 import ProductsPage from './pages/ProductsPage/ProductsPage';
+import ProductPage from './pages/ProductPage/ProductPage';
 import gloves from './assets/data/gloves.json';
 import bats from "./assets/data/bats.json";
 import cleats from "./assets/data/cleats.json";
-import ProductPage from './pages/ProductPage/ProductPage';
+
 
 function App() {
   return (
---
+++ src/utils/api-utils.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const apiURL = "http://localhost:8080";
+
+
+// GET Gloves List
+export const fetchGlovesList = () => {
+    return axios.get(`${apiURL}gloves`);
+};
+
+// GET Specific Glove
+export const fetchSpecificGlove = (selectedGloveId) => {
+    return axios.get(`${apiURL}/gloves/${selectedGloveId}`);
+};
+
+// GET Bats List
+export const fetchBatsList = () => {
+    return axios.get(`${apiURL}/bats`);
+};
+
+// GET Specific Bat
+export const fetchSpecificBat = (selectedBatId) => {
+    return axios.get(`${apiURL}/bats/${selectedBatId}`);
+};
+
+// GET Cleats List
+export const fetchCleatsList = () => {
+    return axios.get(`${apiURL}/cleats`);
+};
+
+// GET Cleats List
+export const fetchSpecificCleat = (selectedCleatId) => {
+    return axios.get(`${apiURL}/cleats/${selectedCleatId}`);
+};
