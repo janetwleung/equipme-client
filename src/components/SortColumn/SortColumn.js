73ecf0e12d702f3b5332b3923f22702129847837
Janet Leung <leung.janetw@hotmail.com>
2022-09-27 21:17:22 -0400

    Disables sort features iin sort column

--- src/components/SortColumn/SortColumn.js
+++ src/components/SortColumn/SortColumn.js
@@ -15,7 +15,7 @@ function SortColumn({ brands, setProductList }) {
                 <span className="sort__list-label">Brands</span>
                 {brands.map(brand => (
                     <li className="sort__brand-list-item" key={brand.id}>
-                        <input type="checkbox" value={brand.brand} id={brand.brand} onChange={(e) => handleToggle(brand.id)}/>
+                        <input type="checkbox" value={brand.brand} id={brand.brand} onChange={(e) => handleToggle(brand.id)} disabled/>
                         <label className="sort__label" htmlFor={brand.brand}>{brand.brand}</label>
                     </li>
                 ))}
@@ -23,23 +23,23 @@ function SortColumn({ brands, setProductList }) {
             <ul className="sort__price-list">
                 <span className="sort__list-label">Price</span>
                 <li className="sort__price-list-item">
-                    <input type="checkbox"/>
+                    <input type="checkbox" disabled/>
                     <label className="sort__label" htmlFor="">less than $100</label>
                 </li>
                 <li className="sort__price-list-item">
-                    <input type="checkbox"/>
+                    <input type="checkbox" disabled/>
                     <label className="sort__label" htmlFor="">$100 - $200</label>
                 </li>
                 <li className="sort__price-list-item">
-                    <input type="checkbox"/>
+                    <input type="checkbox" disabled/>
                     <label className="sort__label" htmlFor="">$200 - $300</label>
                 </li>
                 <li className="sort__price-list-item">
-                    <input type="checkbox"/>
+                    <input type="checkbox" disabled/>
                     <label className="sort__label" htmlFor="">$300 - $400</label>
                 </li>
                 <li className="sort__price-list-item">
-                    <input type="checkbox"/>
+                    <input type="checkbox" disabled/>
                     <label className="sort__label" htmlFor="">$400 - $500</label>
                 </li>
             </ul>
