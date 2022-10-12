90fa833029376e8145be916eb45a65891f616603
Janet Leung <leung.janetw@hotmail.com>
2022-10-11 21:56:35 -0400

    Adds listed out sports manually to disable link

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -2,6 +2,7 @@ import "./Carousel.scss";
 import SportCard from "../SportCard/SportCard";
 
 function Carousel({ sports }) {
+    console.log(sports[0].sport)
     return (
         <div className="carousel" id="carousel">
             <div className="carousel__space">
@@ -20,6 +21,33 @@ function Carousel({ sports }) {
                 />
                 </li>
             ))}
+              {/* <li>
+                    <SportCard
+                    sport={sports[0].sport}
+                    image={sports[0].image}
+                    hoverImage={sports[0].hoverImage}
+                    text="Find Equipment"
+                    link={`/form/${sports[0].id}`}
+                />
+                </li>
+                <li>
+                    <SportCard
+                    sport={sports[1].sport}
+                    image={sports[1].image}
+                    hoverImage={sports[1].hoverImage}
+                    text="Find Equipment"
+                    link="#"
+                />
+                 </li>
+                <li>
+                    <SportCard
+                    sport={sports[2].sport}
+                    image={sports[2].image}
+                    hoverImage={sports[2].hoverImage}
+                    text="Find Equipment"
+                    link="#"
+                />
+                 </li> */}
             </ul>
         </div>
     
